af1b937a39fa929c5b43a6f603880e9b0114c677
wagnermateus <wagnermateus7@gmail.com>
2022-06-27 20:14:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
+import { Router } from "./Router";
+export function App() {
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  );
+}
---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { Event } from "./pages/Event";
+import { Subscriber } from "./pages/Subscriber";
+
+export function Router(){
+    return(
+   <Routes>
+    <Route path="/" element={<Subscriber/>}/>
+    <Route path="/event" element={<Event/>}/>
+    <Route path="/event/lesson/:slug" element={<Event/>}/>
+   </Routes>
+    );
+}
